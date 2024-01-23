d175520879293d3241f0b93583356460c7976382
MoeTutorial <moeraitools@yeah.net>
2024-01-23 15:56:52 +0800

    添加助手函数

---
+++ src/lovely/contract/Arrayable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace lovely\contract;
+
+interface Arrayable
+{
+    public function toArray(): array;
+}
+
+?>
---
+++ src/lovely/contract/Jsonable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace think\contract;
+
+interface Jsonable
+{
+    public function toJson(int $options = JSON_UNESCAPED_UNICODE): string;
+}
+
+?>
